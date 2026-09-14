a92ed63e676bdc2f141d9aa36296a2ef0d8227aa
RangaMokshagna <jayavaramrangamokshagna@gmail.com>
2026-09-14 23:43:07 +0530

    Update shared footer studio credit

--- app/components/site-footer-credit.tsx
+++ app/components/site-footer-credit.tsx
@@ -5,7 +5,7 @@ export function SiteFooterCredit() {
   return (
     <div className="site-footer-credit">
       <p>© {year} Royal Mechanics. All Rights Reserved.</p>
-      <p>Website designed &amp; developed by BR Web Studio.</p>
+      <p>Website designed &amp; developed by B&amp;M Works.</p>
     </div>
   );
 }
